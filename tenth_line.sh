e133f422733fc28d39f6c0acabeb1bcc17d03da4
rookmate <14072042+rookmate@users.noreply.github.com>
2020-03-02 17:07:50 +0000

    [LEETCODE] Bash print 10th line in a file

---
+++ tenth_line.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Runtime: 4 ms, faster than 74.31% of Bash online submissions for Tenth Line.
+# Memory Usage: 3.7 MB, less than 95.24% of Bash online submissions for Tenth Line.
+sed -n 10p file.txt
